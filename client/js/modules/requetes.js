82a5cd89383969aef71472a22f1f9d3b500d7765
Djamel Nait Sidenas <xamel.ns@gmail.com>
2024-12-29 14:33:52 -0500

    clean requetes file

--- client/js/modules/requetes.js
+++ client/js/modules/requetes.js
@@ -2,28 +2,6 @@
 const COCKTAIL_URL = "http://127.0.0.1:3000/cocktails";
 const MEMBER_URL = "http://127.0.0.1:3000/membres";
 
-// const listCocktails = async (page=1, filtres, last= false)=>{
-//   const perPage = isAdmin ? ADMIN_PER_PAGE : FRONT_PER_PAGE;
-//   const queryParams = new URLSearchParams({ page, ...filtres, perPage, last });
-//   const url = `${COCKTAIL_URL}?${queryParams.toString()}`;
-//   try {
-//     const response = await getCocktails(url);
-//     totalPages = response.pagination.totalPages;
-//     currentPage = response.pagination.page;
-//       isAdmin ? afficherListeCocktailsCardsAdmin(response.result)
-//               : afficherListeCocktailsCards(response.result)
-//       paginate(response.pagination, filtres)
-//   } catch (erreur) {
-//     console.log(erreur)
-//     console.log("Erreur s'est produite lors de la requête:");
-//     return [];
-//   }
-// }
-
-
-
-
-
 export const getCocktails = async (url) => {
   const response = await fetch(url);
   return await response.json();
